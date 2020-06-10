df4fca7ff23c0590b6b2046d87a03018d0f07274
nilarrus <ngarciadelshorts@gmail.com>
2020-06-10 19:52:47 +0200

    registre solucionat amb el ranking

--- VMP/app/Http/Controllers/Auth/RegisterController.php
+++ VMP/app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Ranking;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -54,17 +55,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        App\Ranking::create([
-            'user_gm' => $data['email'],
-            'time' => 0,
-            'fails'=> 0,
-            'Lastlevel'=> 0,
-        ]);
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
+        Ranking::create([
+            'user_gm' => $data['email'],
+            'time' => 0,
+            'fails'=> 0,
+            'Lastlevel'=> 0,
+        ]);
+        return $user;
     }
 }
